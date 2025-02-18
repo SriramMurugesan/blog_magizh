85114248982b720c04fb822f0b2154390f43aaa0
SriramMurugesan <sri2014ram@gmail.com>
2025-02-18 20:25:20 +0530

    till 45mins

--- website/auth.py
+++ website/auth.py
@@ -3,7 +3,7 @@ from flask import Blueprint,render_template, redirect, url_for
 auth= Blueprint("auth", __name__) 
 
 
-@auth.route("/Login")
+@auth.route("/login")
 def login():
     return render_template("login.html")
 
@@ -14,4 +14,4 @@ def sign_up():
 
 @auth.route("/logout")
 def logout():
-    return 
+    return redirect(url_for("views.home"))
