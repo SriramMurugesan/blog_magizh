501b2932e4fbade43c599ba0251712db9d331afc
SriramMurugesan <sri2014ram@gmail.com>
2025-02-22 16:28:19 +0530

    complted blog site

--- website/static/index.js
+++ website/static/index.js
@@ -1,10 +1,25 @@
+function getCsrfToken() {
+    return document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+}
+
 function likePost(postId) {
     const likeButton = document.getElementById(`like-button-${postId}`);
     const likeIcon = likeButton.querySelector('i');
     const likeCount = document.getElementById(`like-count-${postId}`);
 
-    fetch(`like-post/${postId}`, {method: "POST"})
-    .then(res => res.json())
+    fetch(`/like-post/${postId}`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': getCsrfToken()
+        }
+    })
+    .then(res => {
+        if (!res.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return res.json();
+    })
     .then(data => {
         if (data.liked) {
             likeIcon.classList.replace('far', 'fas');
